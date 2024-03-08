743a6bf33e7aa83c3b9beba4fb29c3d7e033705a
Pratik Lamichhane <lamichhanepratik9@gmail.com>
2024-03-08 13:03:35 +0545

    basic setup

---
+++ script.js
@@ -0,0 +1,18 @@
+let result = document.getElementById('result');
+let searchBtn = document.getElementById('search-btn');
+let url = "https://api.nationalize.io/?name=";
+
+
+let personName = "samnikal";
+let finalUrl = url + personName;
+
+fetch(finalUrl)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        console.log(data.name);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
